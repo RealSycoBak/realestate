94e95b57047f87992a35b68d672b50173f0fd5ca
Alon Baker <66329736+RealSycoBak@users.noreply.github.com>
2025-02-11 10:20:57 -0500

    Resposnive Designs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,32 @@
 import './Navbar.css';
 import LogoImg from '../assets/ClubLogo.png';
+import { useState } from 'react'; // Add this import
 
-export default function Navbar(){
+export default function Navbar() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-return (
-    <>
-    <div className="Navbar">
-    <div className="imgtop">
-        <img src={LogoImg} alt="realestate" width="100px" height="100px"/>
-    </div>
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
 
-    <nav>
-        <ul>
-        <li><a href="default.asp">HOME</a></li>
-        <li><a href="news.asp">ABOUT</a></li>
-        <li><a href="contact.asp">PORTFOLIO</a></li>
-        <li><a href="about.asp">STUDENTS</a></li>
-        <li><a href="about.asp">NEWS</a></li>
-        </ul>
-    </nav>
-    </div>
-    <div className="line"> </div>
-    </>
-
-)
+    return (
+        <>
+            <div className="Navbar">
+                <div className="imgtop">
+                    <img src={LogoImg} alt="realestate" width="100px" height="100px" />
+                </div>
+                <div className="menu-btn" onClick={toggleMenu}>
+                    <div className="hamburger">☰</div>
+                </div>
+                <ul className={isMenuOpen ? 'active' : ''}>
+                    <li><a href="default.asp">HOME</a></li>
+                    <li><a href="news.asp">ABOUT</a></li>
+                    <li><a href="contact.asp">PORTFOLIO</a></li>
+                    <li><a href="about.asp">STUDENTS</a></li>
+                    <li><a href="about.asp">NEWS</a></li>
+                </ul>
+            </div>
+            <div className="line"> </div>
+        </>
+    )
 }
